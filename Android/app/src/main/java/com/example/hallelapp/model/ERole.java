3da043aa5efa0fbd3638d804e452ef520f25d82b
Lorenzo Aviz dos Santos <lorenzo.aviz250@gmail.com>
2024-04-15 21:52:34 -0400

    finalização da tela de listar eventos

--- Android/app/src/main/java/com/example/hallelapp/model/ERole.java
+++
@@ -1,9 +0,0 @@
-package com.example.hallelapp.model;
-
-public enum ERole {
-
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_ASSOCIADO,
-
-}
--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.hallelapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.hallelapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -26,6 +26,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    viewBinding{
+        enable = true
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
@@ -34,6 +42,24 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    //DEPDENCIAS ADICIONADAS  NO PROJETO
+
+    //**Lombok para adicionar anotações para facilitar a codificação
+    implementation("org.projectlombok:lombok:1.18.20")
+
+
+
+    //** Dependencias para acessar a API
+
+    // Apache OkHttp e Gson
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("com.squareup.okhttp3:okhttp:4.9.2")
+    implementation(libs.annotation)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
