b77ba7bb115df546861aaa0302aa3217b7b85e7c
Gisele Saraiva Vieira <125830783+giselesaraiva@users.noreply.github.com>
2024-06-03 20:48:04 -0400

    12

--- MyApplication/app/src/main/java/com/example/hallelapp/activity/DoarpEvento.java
+++ MyApplication/app/src/main/java/com/example/hallelapp/activity/DoarpEvento.java
@@ -12,7 +12,7 @@ public class DoarpEvento extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ser_voluntario);
+        setContentView(R.layout.activity_servoluntario);
     }
 
 }
--- MyApplication/app/src/main/java/com/example/hallelapp/activity/FormVoluntarioActivity.java
+++ MyApplication/app/src/main/java/com/example/hallelapp/activity/FormVoluntarioActivity.java
@@ -12,6 +12,6 @@ public class FormVoluntarioActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ser_voluntario);
+        setContentView(R.layout.activity_servoluntario);
     }
 }
